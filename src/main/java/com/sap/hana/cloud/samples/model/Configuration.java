1387f5c0912aa0a82d116e05dcefe9a5001f46a7
I043324 <I043324@INLN50888500A.apj.global.corp.sap>
2016-12-15 12:35:01 +0900

    Making changes

--- src/main/java/com/sap/hana/cloud/samples/model/Configuration.java
+++ src/main/java/com/sap/hana/cloud/samples/model/Configuration.java
@@ -8,28 +8,30 @@ public class Configuration {
 	private static final String PROXY_HOST = "proxy";
 	private static final int PROXY_PORT = 8080;
 
-	private final String user = "my_username";
-	private final String password = "my_password";
+	private final String user = "<Enter your user>";
+	private final String password = "<Enter your password>";
+	
 
 	private final List<ApplicationConfiguration> appsList = new ArrayList<ApplicationConfiguration>();
 
 	public void configure() {
 
-		String landscapeFQDN1 = "api.hana.ondemand.com";
-		String account1 = "a1";
-		String application1 = "app1";
+		String landscapeFQDN1 = "hanatrial.ondemand.com";
+		String account1 = "<Enter your trial account name>";
+		String application1 = "<Enter your application name>";
 		ApplicationConfiguration app1Config = new ApplicationConfiguration(application1, account1, landscapeFQDN1);
 		this.appsList.add(app1Config);
 
-		String landscapeFQDN2 = "api.us1.hana.ondemand.com";
-		String account2 = "a2";
-		String application2 = "app2";
-		ApplicationConfiguration app2Config = new ApplicationConfiguration(application2, account2, landscapeFQDN2);
-		this.appsList.add(app2Config);
+//		String landscapeFQDN2 = "hanatrial.ondemand.com";
+//		String account2 = "";
+//		String application2 = "weatherapplication";
+//		ApplicationConfiguration app2Config = new ApplicationConfiguration(application2, account2, landscapeFQDN2);
+//		this.appsList.add(app2Config);
 	}
 
 	public String getProxyHost() {
-		return Configuration.PROXY_HOST;
+		return null;
+//		return Configuration.PROXY_HOST;
 	}
 
 	public int getProxyPort() {
